8ca230942e587163c30c880b6eb322065be4e290
waqaskhan <mwaqas3030@gmail.com>
2021-05-06 15:55:01 +0500

    make it better

--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ const bodyparser = require("body-parser");
 const router = require('../controller/routes');
 const app = express();
 app.use(cors());
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:"true"}));
 const port = process.env.PORT || 1000;
 app.use("/user",router)
-app.get("/", (req, res) => {
-  res.send("welcome to hotel management website");
-});
-
 app.listen(port, () => { 
   console.log(`server is listening at port ${port}`);
 });
-
